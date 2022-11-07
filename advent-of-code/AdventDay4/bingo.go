3273821f71592ce03b95e88d3d945f900b46a0fd
Paul Wagner DT <paul.wagner@telekom.de>
2022-11-07 18:57:04 +0100

    Giant Squid Bingo part 1 solved Score of winning board =  16716

--- advent-of-code/AdventDay4/bingo.go
+++ advent-of-code/AdventDay4/bingo.go
@@ -1,28 +1,29 @@
 /*
 --- Day 4: Giant Squid ---
 
-You're already almost 1.5km (almost a mile) below the surface of the ocean, already so deep that you can't see any sunlight. What you can see, however, is a giant squid that has attached itself to the outside of your submarine.
+You're already almost 1.5km (almost a mile) below the surface of the ocean, already so deep that you can't see any sunlight.
+What you can see, however, is a giant squid that has attached itself to the outside of your submarine.
 
 Maybe it wants to play bingo?
 
-Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. Numbers are chosen at random, and the chosen number is marked on all boards on which it appears. (Numbers may not appear on all boards.) If all numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. Numbers are chosen at random,
+and the chosen number is marked on all boards on which it appears. (Numbers may not appear on all boards.)
+If all numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)
 
-The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time. It automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input). For example:
+The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time.
+It automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input).
+For example:
 
 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
-
-	8  2 23  4 24
-
+8  2 23  4 24
 21  9 14 16  7
+6 10  3 18  5
+1 12 20 15 19
 
-	6 10  3 18  5
-	1 12 20 15 19
-
-	3 15  0  2 22
-	9 18 13 17  5
-
+3 15  0  2 22
+9 18 13 17  5
 19  8  7 25 23
 20 11 10 24  4
 14 21 16 12  6
@@ -31,47 +32,40 @@ The submarine has a bingo subsystem to help passengers (currently, you and the g
 10 16 15  9 19
 18  8 23 26 20
 22 11 13  6  5
-
-	2  0 12  3  7
+2  0 12  3  7
 
 After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no winners, but the boards are marked as follows (shown here adjacent to each other to save space):
 
-22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
-
-	8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
-
-21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
-
-	6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
-	1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+22 13 17 11  0         	3 15  0  2 22        	14 21 17 24  4
+8  2 23  4 24         	9 18 13 17  5        	10 16 15  9 19
+21  9 14 16  7        	19  8  7 25 23        	18  8 23 26 20
+6 10  3 18  5        	20 11 10 24  4        	22 11 13  6  5
+1 12 20 15 19        	14 21 16 12  6         	2  0 12  3  7
 
 After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are still no winners:
 
-22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
-
-	8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
-
-21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
-
-	6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
-	1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+22 13 17 11  0         	3 15  0  2 22        	14 21 17 24  4
+8  2 23  4 24         	9 18 13 17  5        	10 16 15  9 19
+21  9 14 16  7        	19  8  7 25 23        	18  8 23 26 20
+6 10  3 18  5        	20 11 10 24  4        	22 11 13  6  5
+1 12 20 15 19        	14 21 16 12  6         	2  0 12  3  7
 
 Finally, 24 is drawn:
 
-22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+22 13 17 11  0         	3 15  0  2 22        	14 21 17 24  4
+8  2 23  4 24         	9 18 13 17  5        	10 16 15  9 19
+21  9 14 16  7        	19  8  7 25 23        	18  8 23 26 20
+6 10  3 18  5        	20 11 10 24  4        	22 11 13  6  5
+1 12 20 15 19        	14 21 16 12  6         	2  0 12  3  7
 
-	8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+At this point, the third board wins because it has at least one complete row or column
+of marked numbers (in this case, the entire top row is marked: 14 21 17 24 4).
 
-21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+The score of the winning board can now be calculated. Start by finding the sum of all
+unmarked numbers on that board; in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board won, 24, to get the final score, 188 * 24 = 4512.
 
-	6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
-	1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
-
-At this point, the third board wins because it has at least one complete row or column of marked numbers (in this case, the entire top row is marked: 14 21 17 24 4).
-
-The score of the winning board can now be calculated. Start by finding the sum of all unmarked numbers on that board; in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board won, 24, to get the final score, 188 * 24 = 4512.
-
-To guarantee victory against the giant squid, figure out which board will win first. What will your final score be if you choose that board?
+To guarantee victory against the giant squid, figure out which board will win first.
+What will your final score be if you choose that board?
 */
 package main
 
@@ -117,13 +111,13 @@ func main() {
 			continue
 		}
 
-		if inBoardStringIdx < 5 {
+		if inBoardStringIdx < 5 { // always read 5 lines into the array "inBoardStrings"
 			inBoardStrings[inBoardStringIdx] = lineContent
 			inBoardStringIdx++
 		}
-		if inBoardStringIdx == 5 {
+		if inBoardStringIdx == 5 { // once we reach the 5th line of strings -> convert the 5 strings into a board - a 5x5 array of ints
 			board = stringsToBoard(inBoardStrings)
-			boards = append(boards, board)
+			boards = append(boards, board) // and add the board to the slice of all bingo boards
 			boardNumber++
 			fmt.Println("board #", boardNumber, " is : ", board)
 			inBoardStringIdx = 0 // restart idx for next board
@@ -138,10 +132,12 @@ func main() {
 
 	// fmt.Println("result for boards : ", boards)
 
-	// solve bingo ...
+	// solve bingo ... : call bingo with each int in inValues and the pointer to the slice of bingo-boards
+	// bingo() will detect when the first board is solved.
 	for _, num := range inValues {
 		fmt.Print("\ncalling bingo = ", num, " : ")
 		bingo(&boards, num)
+
 	}
 }
 
@@ -173,18 +169,36 @@ func stringsToBoard(inStrings [5]string) (brd boardType) {
 }
 
 func bingo(boards *[]boardType, number int) {
-	for boardIdx, board := range *boards {
-		//board:=&boards[boardIdx]
-		elimNumInBoard(&board, number) ////// PROBLEM - MUST CALL by Reference to MODIFY the original Slice-data !!!!
-		win := checkBoardBingo(&board)
+	//	fmt.Printf("len boards ? %T,%d", boards, len(*boards))
+	for boardIdx := 0; boardIdx < len(*boards); boardIdx++ {
+		elimNumInBoard(&(*boards)[boardIdx], number) // call func with the pointer to the board with "boardIdx" inside the global slice "boards"
+
+		if boardIdx == 0 {
+			fmt.Println("display board at boardIndex = ", boardIdx, " board = ", (*boards)[boardIdx])
+		}
+		win := checkBoardBingo(&(*boards)[boardIdx])
 		if win {
-			fmt.Println("Found the winning board at boardIndex = ", boardIdx, " board = ", board)
+			fmt.Println("Found the winning board at boardIndex = ", boardIdx, " board = ", (*boards)[boardIdx])
+
+			fmt.Println("Score of winning board = ", calcScore(&(*boards)[boardIdx], number))
+
 			panic("Stop on Bingo !")
 		}
-		if boardIdx == 20 {
-			fmt.Println("display board at boardIndex = ", boardIdx, " board = ", board)
-		}
 	}
+	/*	for boardIdx, board := range *boards {
+			//board:=&boards[boardIdx]
+			//	fmt.Printf("what is board ? %T", &(boards[boardIdx]))
+			elimNumInBoard(&board, number) ////// PROBLEM - MUST CALL by Reference to MODIFY the original Slice-data !!!!
+			win := checkBoardBingo(&board)
+			if win {
+				fmt.Println("Found the winning board at boardIndex = ", boardIdx, " board = ", board)
+				panic("Stop on Bingo !")
+			}
+			if boardIdx == 0 {
+				fmt.Println("display board at boardIndex = ", boardIdx, " board = ", board)
+			}
+		}
+	*/
 	return
 }
 
@@ -193,7 +207,7 @@ func elimNumInBoard(board *boardType, number int) {
 		for rowIdx, cell := range line {
 			if cell == number {
 				board[lineIdx][rowIdx] = 0 // if a cell matches the called "bingo number" then set it to zero
-				fmt.Print("Match")
+				// fmt.Print("Match")
 			}
 		}
 	}
@@ -226,4 +240,14 @@ func checkBoardBingo(board *boardType) bool {
 
 	}
 	return false
+}
+
+func calcScore(board *boardType, number int) (result int) {
+	for lineIdx, line := range board {
+		for rowIdx, _ := range line {
+			result += board[lineIdx][rowIdx]
+		}
+	}
+
+	return result * number
 }
